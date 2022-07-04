83b6a3a1f8c482b8b7a327978b4dcec4316ae9f4
anshkush92college <imt_2020013@iiitm.ac.in>
2022-07-04 22:51:02 +0530

    Initialized and the modified the react project + typescript

---
+++ src/components/Component.tsx
@@ -0,0 +1,14 @@
+// Test -------------------------- Importing the Packages ---------------------------------
+
+// Test -------------------------- Importing the styles / other components ----------------
+
+// Test -------------------------- Structure of Props ----------------------------------
+
+// Test -------------------------- The current component ----------------------------------
+const Component = () => {
+  return <div>Component</div>;
+};
+
+
+// Test -------------------------- Exporting the current component ------------------------
+export default Component;
